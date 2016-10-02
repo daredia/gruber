60ea14c1c117f5050f061b90b4c09bfdf5eb1f55
Shehzad Daredia <shehzad.daredia@gmail.com>
2016-10-01 20:07:33 -0700

    Set up basic server and dummy client

---
+++ server/config.js
@@ -0,0 +1,15 @@
+// MODULES ==========================================
+var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+
+var app = express();
+
+// CONFIGURATION ====================================
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/../client'));
+app.use(express.static(__dirname + '/../node_modules'));
+
+module.exports = app;
---
+++ server/index.js
@@ -0,0 +1,6 @@
+var app = require('./config');
+var port = process.env.PORT || 4568;
+
+app.listen(port);
+
+console.log('Server now listening on port', port);
