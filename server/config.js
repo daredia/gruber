0cb1c89e9769f655f7dacf5b54fed9b7a7b32a3e
Shehzad Daredia <shehzad.daredia@gmail.com>
2016-10-07 10:12:20 -0700

    Resume work

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
-// MODULES ============================================
+// MODULES ===========================================
 var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
@@ -6,14 +6,14 @@ var api = require('./routes/api');
 
 var app = express();
 
-// CONFIGURATION ======================================
+// CONFIGURATION =====================================
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/../client'));
 app.use(express.static(__dirname + '/../node_modules'));
 
-// ROUTES =============================================
+// ROUTES ============================================
 app.use('/api', api);
 
 module.exports = app;
